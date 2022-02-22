4eb5969caa5acac56c30db8e41010c29c33f5edd
Ignacio Vargas <ignaevc@gmail.com>
2022-02-22 14:27:51 -0600

    Changed out csv parsing to use numpy

--- tarea1.py
+++ tarea1.py
@@ -1,10 +1,20 @@
 import csv
+import numpy as np
 
 def main():
-    with open('Beijing.csv', newline='') as csvfile:
-        csvreader = csv.reader(csvfile) 
-        for row in csvreader:
-            print(row)
+    # Variable que queremos analizar
+    lws = 15
 
+    # Obtener datos de .csv
+    csvdata = np.genfromtxt('Beijing.csv', 
+            skip_header=1, 
+            missing_values='NA', 
+            delimiter=',', 
+            autostrip=True, 
+            usecols=lws)
+    
+    # Remover valores inválidos
+    clean_csvdata = csvdata[np.logical_not(np.isnan(csvdata))]
+    
 if __name__ == "__main__":
     main()
