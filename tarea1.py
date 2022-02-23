9f0ae32b257704f432b1fd510d8b16f5b506059a
Ignacio Vargas <ignaevc@gmail.com>
2022-02-22 19:44:14 -0600

    Added histogram

--- tarea1.py
+++ tarea1.py
@@ -1,6 +1,11 @@
 import numpy as np
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt, ticker
 import math
+
+from time import time
+
+np.set_printoptions(threshold=10)
+BIN_PERCENT = 0.05
 
 def main():
     # Variable que queremos analizar
@@ -17,6 +22,7 @@ def main():
     # Remover valores inválidos
     clean_csvdata = np.sort(csvdata[np.logical_not(np.isnan(csvdata))])
     length = len(clean_csvdata)
+    print("Datos válidos:",clean_csvdata)
     print("Cantidad de datos válidos: {}".format(length))
 
     # Mediana
@@ -76,9 +82,47 @@ def main():
     ax_box.set_title('Diagrama de cajas')
     ax_box.set_xlabel('lws')
     ax_box.set_yticks([])
+    ax_box.set_xscale('log')
+    #ax_box.set_xticks([1,5,10,50,100, 200, 500])
+    #ax_box.get_xaxis().get_major_formatter().labelOnlyBase = False
+    #ax_box.get_xaxis().get_major_formatter().labelOnlyBase = False
+    ax_box.set_xticks([0.5, 1, 5, 10, 50, 100, 500])
+    ax_box.get_xaxis().set_major_formatter(ticker.ScalarFormatter())
 
     fig_box.savefig("diagrama_cajas.svg")
-    
+
+    # Histogram
+    bins = math.ceil(math.sqrt(length))
+    width = (sample_range*(1+BIN_PERCENT))/bins
+
+    # Para los límites se toma un 5% adicional del rango para tener valores "raros" que no choquen con los valores medidos
+    minval = clean_csvdata.min()-BIN_PERCENT*sample_range/2
+    limits = np.fromfunction(lambda i: i*width + minval, (bins+1,))
+    #limits = np.array([x*width + minval for x in range(bins+1)])
+
+    # Revisar que los límites no choquen con los valores:
+    intersection = np.intersect1d(clean_csvdata, limits)
+
+    if len(intersection > 0):
+        raise Exception("Algunos límites del histograma chocan con algunos de los valores medidos. Ocurre con los siguientes valores/límites {}".format(intersection))
+
+    def bin_freq(i):
+        return len(np.where((clean_csvdata > limits[i]) & (clean_csvdata < limits[i+1]))[0])
+
+    freq = np.vectorize(bin_freq)(np.arange(bins))
+
+    fig_hist, ax_hist = plt.subplots()
+    ax_hist.bar(limits[:-1], freq, width=width, align='edge')
+    #ax_hist.set_yscale('log')
+    ax_hist.set_title('Histograma')
+    ax_hist.set_xlabel('lws')
+    ax_hist.set_ylabel('Frecuencia')
+
+    plt.plot([mean, mean], [0, 2e4],color='red',
+         linestyle='dashed',linewidth=2)  # plotting the arbitrary line from point (25,10) to (65,45).
+
+    fig_hist.savefig("histograma.svg")
+
 if __name__ == "__main__":
     main()
-    plt.show()
+    #plt.show()
